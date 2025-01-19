73e516c956e66644cfd03e6ca389366e8373c96f
lloydrichards <lloyd.d.richards@gmail.com>
2025-01-19 09:48:52 +0100

    refactor: :fire: remove examples

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 import * as React from "react";
 
-import { OpenInV0Button } from "@/components/open-in-v0-button";
-import PokemonPage from "@/registry/complex-component/page";
-import { ExampleForm } from "@/registry/example-form/example-form";
-import { HelloWorld } from "@/registry/hello-world/hello-world";
-
-// This page displays items from the custom registry.
-// You are free to implement this with your own design as needed.
-
 export default function Home() {
   return (
     <div className="max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8">
@@ -17,43 +9,7 @@ export default function Home() {
           A custom registry for distribution code using shadcn.
         </p>
       </header>
-      <main className="flex flex-col flex-1 gap-8">
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A simple hello world component
-            </h2>
-            <OpenInV0Button name="hello-world" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
-            <HelloWorld />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A contact form with Zod validation.
-            </h2>
-            <OpenInV0Button name="example-form" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[500px] relative">
-            <ExampleForm />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A complex component showing hooks, libs and components.
-            </h2>
-            <OpenInV0Button name="complex-component" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
-            <PokemonPage />
-          </div>
-        </div>
-      </main>
+      <main className="flex flex-col flex-1 gap-8"></main>
     </div>
   );
 }
