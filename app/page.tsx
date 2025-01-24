aba5fba770ca4788de94b79154b86ef1804ae53b
lloydrichards <lloyd.d.richards@gmail.com>
2025-01-24 22:58:29 +0100

    refactor: :speech_balloon: add footer with links

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Code2 } from "lucide-react";
 import * as React from "react";
 
 const Home = async () => {
@@ -24,39 +25,55 @@ const Home = async () => {
           A collection of stories for the components of Shadcn/ui
         </p>
       </header>
-      <Table className="table-fixed">
-        <TableCaption>A list of all registry items</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-40">Name</TableHead>
-            <TableHead className="w-20">link</TableHead>
-            <TableHead>cmd</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {registry.items.map((item) => (
-            <TableRow key={item.name}>
-              <TableCell className="w-50 font-medium">{item.title}</TableCell>
-              <TableCell>
-                <Button variant="link" asChild>
-                  <a
-                    target="_blank"
-                    href={`https://registry.lloydrichards.dev/registry/${item.name}`}
-                  >
-                    Link
-                  </a>
-                </Button>
-              </TableCell>
-              <TableCell>
-                <CommandBlock
-                  command={`npx shadcn@latest add https://registry.lloydrichards.dev/registry/${item.name}`}
-                />
-              </TableCell>
+      <main className="flex flex-1 flex-col gap-8">
+        <Table className="table-fixed">
+          <TableCaption>A list of all registry items</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-40">Name</TableHead>
+              <TableHead className="w-20">link</TableHead>
+              <TableHead>cmd</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <main className="flex flex-1 flex-col gap-8"></main>
+          </TableHeader>
+          <TableBody>
+            {registry.items.map((item) => (
+              <TableRow key={item.name}>
+                <TableCell className="w-50 font-medium">{item.title}</TableCell>
+                <TableCell>
+                  <Button variant="link" asChild>
+                    <a
+                      target="_blank"
+                      href={`https://registry.lloydrichards.dev/registry/${item.name}`}
+                    >
+                      Link
+                    </a>
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <CommandBlock
+                    command={`npx shadcn@latest add https://registry.lloydrichards.dev/registry/${item.name}`}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
+      <footer className="flex flex-row items-center justify-between">
+        <Button variant="link" asChild>
+          <a href="https://www.lloydrichards.dev" target="_blank">
+            lloydrichards.dev
+          </a>
+        </Button>
+        <Button variant="link" asChild>
+          <a
+            href="https://github.com/lloydrichards/shadcn-storybook-registry"
+            target="_blank"
+          >
+            <Code2 size={24} /> Repo
+          </a>
+        </Button>
+      </footer>
     </div>
   );
 };
