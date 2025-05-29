37bfad53db19cd13868f42155fad2514e2d74dab
Lloyd Richards <59728961+lloydrichards@users.noreply.github.com>
2025-05-29 19:57:19 +0200

    feat: 🧪 add interaction test for collapsable story

--- registry/collapsible.stories.tsx
+++ registry/collapsible.stories.tsx
@@ -7,6 +7,7 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
+import { expect, userEvent, waitFor, within } from "storybook/test";
 
 /**
  * An interactive component which expands/collapses a panel.
@@ -52,5 +53,24 @@ export const Default: Story = {};
 export const Disabled: Story = {
   args: {
     disabled: true,
+  },
+};
+
+export const OpenAndClose: Story = {
+  name: "when collapsable trigger is clicked, should show content",
+  tags: ["!dev", "!autodocs"],
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const trigger = await canvas.findByRole("button");
+
+    // Open the collapsible
+    await userEvent.click(trigger, { delay: 100 });
+    const collapsableContent = await waitFor(() =>
+      canvasElement.querySelector('[data-slot="collapsable content"]'),
+    );
+
+    // Close the collapsible
+    await userEvent.click(trigger, { delay: 100 });
+    await waitFor(() => expect(collapsableContent).toBeNull());
   },
 };
