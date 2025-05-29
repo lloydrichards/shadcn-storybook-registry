dc693510e20e5c99deae508044242c11bcef9412
Lloyd Richards <59728961+lloydrichards@users.noreply.github.com>
2025-05-29 19:58:23 +0200

    feat: 🧪 add interaction test for drawer story

--- registry/drawer.stories.tsx
+++ registry/drawer.stories.tsx
@@ -11,6 +11,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { expect, fn, userEvent, within } from "storybook/test";
 
 /**
  * A drawer component for React.
@@ -19,7 +20,11 @@ const meta = {
   title: "ui/Drawer",
   component: Drawer,
   tags: ["autodocs"],
-  argTypes: {},
+  args: {
+    onOpenChange: fn(),
+    onClose: fn(),
+    onAnimationEnd: fn(),
+  },
   render: (args) => (
     <Drawer {...args}>
       <DrawerTrigger>Open</DrawerTrigger>
@@ -29,12 +34,10 @@ const meta = {
           <DrawerDescription>This action cannot be undone.</DrawerDescription>
         </DrawerHeader>
         <DrawerFooter>
-          <button className="bg-primary text-primary-foreground rounded px-4 py-2">
+          <DrawerClose className="bg-primary text-primary-foreground rounded px-4 py-2">
             Submit
-          </button>
-          <DrawerClose>
-            <button className="hover:underline">Cancel</button>
           </DrawerClose>
+          <DrawerClose className="hover:underline">Cancel</DrawerClose>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
@@ -42,7 +45,7 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-} as Meta<typeof Drawer>;
+} satisfies Meta<typeof Drawer>;
 
 export default meta;
 
@@ -51,4 +54,52 @@ type Story = StoryObj<typeof meta>;
 /**
  * The default form of the drawer.
  */
-export const Default: Story = {} as Story;
+export const Default: Story = {};
+
+export const ShouldOpenCloseSubmit: Story = {
+  name: "when clicking Submit button, should close the drawer",
+  tags: ["!dev", "!autodocs"],
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement.ownerDocument.body);
+
+    // Open the dialog
+    await userEvent.click(await canvas.findByRole("button", { name: "Open" }));
+    await expect(args.onOpenChange).toHaveBeenCalled();
+
+    const dialog = await canvas.findByRole("dialog");
+    expect(dialog).toBeInTheDocument();
+    expect(dialog).toHaveAttribute("data-state", "open");
+
+    // Close the dialog
+    await userEvent.click(
+      await canvas.findByRole("button", { name: "Submit" }),
+      { delay: 100 },
+    );
+    await expect(args.onClose).toHaveBeenCalled();
+    expect(dialog).toHaveAttribute("data-state", "closed");
+  },
+};
+
+export const ShouldOpenCloseCancel: Story = {
+  name: "when clicking Cancel button, should close the drawer",
+  tags: ["!dev", "!autodocs"],
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement.ownerDocument.body);
+
+    // Open the dialog
+    await userEvent.click(await canvas.findByRole("button", { name: "Open" }));
+    await expect(args.onOpenChange).toHaveBeenCalled();
+
+    const dialog = await canvas.findByRole("dialog");
+    expect(dialog).toBeInTheDocument();
+    expect(dialog).toHaveAttribute("data-state", "open");
+
+    // Close the dialog
+    await userEvent.click(
+      await canvas.findByRole("button", { name: "Cancel" }),
+      { delay: 100 },
+    );
+    await expect(args.onClose).toHaveBeenCalled();
+    expect(dialog).toHaveAttribute("data-state", "closed");
+  },
+};
